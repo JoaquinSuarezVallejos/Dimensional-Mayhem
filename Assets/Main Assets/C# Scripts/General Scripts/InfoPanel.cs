96097fe31d5c26430ff05fd81c25372536491b3b
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-11-28 10:01:59 -0300

    Info panel finished

--- Assets/Main Assets/C# Scripts/General Scripts/InfoPanel.cs
+++ Assets/Main Assets/C# Scripts/General Scripts/InfoPanel.cs
@@ -3,29 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InfoPanel : MonoBehaviour
 {
     float currentHealth;
     EmeraldAIPlayerHealth healthScript;
 
-    float currentTime;
+    string currentTime;
     TimeElapsed timeScript;
 
     string currentLevel;
 
+    Text panelText;
 
     // Start is called before the first frame update
     void Start()
     {
         healthScript = GameObject.Find("PlayerController").GetComponent<EmeraldAIPlayerHealth>();
         timeScript = GameObject.Find("TIME ELAPSED").GetComponent<TimeElapsed>();
-        currentLevel = SceneManager.
+        currentLevel = SceneManager.GetActiveScene().name;
+        panelText = GameObject.Find("Panel Text").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentHealth = healthScript.CurrentHealth;
+        currentTime = timeScript.currentTime;
+        panelText.text = "Health: " + currentHealth.ToString() + "\n" + "Time elapsed: " + currentTime + "\n" + currentLevel;
+
     }
 }
