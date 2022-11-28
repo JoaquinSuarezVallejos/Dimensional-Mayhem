58b8258ba5c625acffe738f8786d914c146a28af
JoacoSV <joaquin.suarezvallejos@gmail.com>
2022-11-28 20:08:25 -0300

    HealthDisplay & More Finished

--- Assets/Main Assets/C# Scripts/General Scripts/HealthDisplay.cs
+++ Assets/Main Assets/C# Scripts/General Scripts/HealthDisplay.cs
@@ -7,14 +7,16 @@ using EmeraldAI.Example;
 public class HealthDisplay : MonoBehaviour
 {
     int currentHealth;
-    public GameObject textMeshPro_HealthText;
+    int maxPlayerHealth = 160;
+    public GameObject textMeshPro_HealthText, textMeshPro_HealthSprite;
     EmeraldAIPlayerHealth healthScript;
-    TextMeshProUGUI HealthDisplayText;
-
+    TextMeshProUGUI HealthDisplayText, HealthSprite;
 
     void Start()
     {
         HealthDisplayText = textMeshPro_HealthText.GetComponent<TextMeshProUGUI>();
+        HealthSprite = textMeshPro_HealthSprite.GetComponent<TextMeshProUGUI>();
+
         healthScript = GameObject.Find("PlayerController").GetComponent<EmeraldAIPlayerHealth>();
     }
 
@@ -22,5 +24,25 @@ public class HealthDisplay : MonoBehaviour
     {
         currentHealth = healthScript.CurrentHealth;
         HealthDisplayText.text = currentHealth.ToString();
+
+        if (currentHealth <= maxPlayerHealth && currentHealth >= 75)
+        {
+            HealthSprite.text = "<sprite name=\"Heart1\">";
+        }
+
+        else if (currentHealth <= 75 && currentHealth >= 50)
+        {
+            HealthSprite.text = "<sprite name=\"Heart2\">";
+        }
+
+        else if (currentHealth <= 50 && currentHealth >= 25)
+        {
+            HealthSprite.text = "<sprite name=\"Heart3\">";
+        }
+
+        else 
+        {
+            HealthSprite.text = "<sprite name=\"Heart4\">";
+        }
     }
 }
