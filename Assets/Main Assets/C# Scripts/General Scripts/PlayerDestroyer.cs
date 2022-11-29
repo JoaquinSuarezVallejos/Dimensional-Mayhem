16572610fffe44ebc728f8fe9fe9453fc5cd44c1
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-11-29 14:06:55 -0300

    Death scene fixed bug
    player cannot move before scene starting

--- Assets/Main Assets/C# Scripts/General Scripts/PlayerDestroyer.cs
+++ Assets/Main Assets/C# Scripts/General Scripts/PlayerDestroyer.cs
@@ -7,6 +7,8 @@ public class PlayerDestroyer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(GameObject.Find("PLAYER SAVER"));   
+        Destroy(GameObject.Find("PLAYER SAVER"));
+        GameObject.Find("PlayerController").transform.position = GameObject.FindGameObjectWithTag("Spawner").transform.position;
+        GameObject.Find("PlayerController").transform.rotation = GameObject.FindGameObjectWithTag("Spawner").transform.rotation;
     }
 }
