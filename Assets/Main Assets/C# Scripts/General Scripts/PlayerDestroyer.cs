ed037a6f730e800581bfa3583c737f27d7e215f6
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-11-29 15:23:53 -0300

    Death scene bug fixing attempt 2

--- Assets/Main Assets/C# Scripts/General Scripts/PlayerDestroyer.cs
+++ Assets/Main Assets/C# Scripts/General Scripts/PlayerDestroyer.cs
@@ -8,7 +8,7 @@ public class PlayerDestroyer : MonoBehaviour
     void Start()
     {
         Destroy(GameObject.Find("PLAYER SAVER"));
-        GameObject.Find("PlayerController").transform.position = GameObject.FindGameObjectWithTag("Spawner").transform.position;
-        GameObject.Find("PlayerController").transform.rotation = GameObject.FindGameObjectWithTag("Spawner").transform.rotation;
+        GameObject.Find("CenterEyeAnchor").transform.position = GameObject.FindGameObjectWithTag("Spawner").transform.position;
+        GameObject.Find("CenterEyeAnchor").transform.rotation = GameObject.FindGameObjectWithTag("Spawner").transform.rotation;
     }
 }
