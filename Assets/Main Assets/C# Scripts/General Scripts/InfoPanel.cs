d4c440aa46463c5c500309ec3e6d7df0f552e0c3
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-11-29 10:45:17 -0300

    Finished info panel

--- Assets/Main Assets/C# Scripts/General Scripts/InfoPanel.cs
+++ Assets/Main Assets/C# Scripts/General Scripts/InfoPanel.cs
@@ -8,26 +8,52 @@ using TMPro;
 
 public class InfoPanel : MonoBehaviour
 {
-    float currentHealth;
-    EmeraldAIPlayerHealth healthScript;
-    string currentTime;
+    #region Scripts
     TimeElapsed timeScript;
+    EmeraldAIPlayerHealth healthScript;
+    GameObject[] enemiesArray;
+    KillCounter playerKills;
+    #endregion
+    #region Info
+    float currentHealth;
+    string currentTime;
     string currentLevel;
+    int enemiesLeft;
+    int totalEnemies;
+    #endregion
+
+    #region Texts
+    [SerializeField] TextMeshProUGUI healthText;
+    [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] TextMeshProUGUI enemiesText;
+    [SerializeField] TextMeshProUGUI levelText;
+    #endregion
 
     void Start()
     {
         healthScript = GameObject.Find("PlayerController").GetComponent<EmeraldAIPlayerHealth>();
         timeScript = GameObject.Find("TIME ELAPSED").GetComponent<TimeElapsed>();
         currentLevel = SceneManager.GetActiveScene().name;
-        //panelText = GameObject.Find("Panel Text").GetComponent<Text>();
+        playerKills = GameObject.Find("ENEMY KILLS COUNTER").GetComponent<KillCounter>();
+        enemiesArray = GameObject.FindGameObjectsWithTag("Respawn");
+
+        healthText = GameObject.Find("Health (TMP)").GetComponent<TextMeshProUGUI>();
+        timeText = GameObject.Find("Clock (TMP)").GetComponent<TextMeshProUGUI>();
+        enemiesText = GameObject.Find("Enemies (TMP)").GetComponent<TextMeshProUGUI>();
+        levelText = GameObject.Find("Level (TMP)").GetComponent<TextMeshProUGUI>();
+
+        totalEnemies = enemiesArray.Length;
     }
 
     void Update()
     {
+        enemiesLeft = totalEnemies - playerKills.kills;
         currentHealth = healthScript.CurrentHealth;
         timeScript.GetCurrentTime();
         currentTime = timeScript.currentTime;
-        //panelText.text = "Health: " + currentHealth.ToString() + "\n" + "Time elapsed: " + currentTime + "\n" + currentLevel;
-        Debug.Log("Health: " + currentHealth.ToString() + "\n" + "Time elapsed: " + currentTime + "\n" + currentLevel);
+        healthText.text = "<sprite index= 0> " + currentHealth.ToString();
+        timeText.text = "<sprite index= 0> " + currentTime;
+        enemiesText.text = "<sprite index= 0> " + enemiesLeft.ToString();
+        levelText.text = "<sprite index= 0> " + currentLevel;
     }
 }
