3beae96f3a3b37d7671a94de9e8c28576c3bdc63
JoacoSV <joaquin.suarezvallejos@gmail.com>
2022-12-05 18:34:00 -0300

    Final Updates V26

--- Assets/Main Assets/C# Scripts/General Scripts/TimeMachineButtonDown.cs
+++ Assets/Main Assets/C# Scripts/General Scripts/TimeMachineButtonDown.cs
@@ -4,18 +4,39 @@ using UnityEngine;
 
 public class TimeMachineButtonDown : MonoBehaviour
 {
-    public GameObject electricityEffect, finalBoss, wallFinalBoss1, wallFinalBoss2, missionCanvas;
+    public GameObject timeMachine, electricityEffect, finalBoss, bossSpawnEffect, wallFinalBoss1, wallFinalBoss2, missionCanvas, flames, initializeTxt, errorTxt;
     public AudioSource finalBossMusic, theVillageMusic;
 
     public void buttonPressed()
     {
+        missionCanvas.SetActive(false);
         electricityEffect.SetActive(true);
-        finalBoss.SetActive(true);
+        initializeTxt.SetActive(false);
+        StartCoroutine("errorTxtBlink1");
+        timeMachine.GetComponent<Animator>().Play("TimeMachineError");
+
+        finalBoss.SetActive(true); 
+        bossSpawnEffect.SetActive(true);
+        flames.SetActive(true);
         wallFinalBoss1.SetActive(true);
         wallFinalBoss2.SetActive(true);
+
         theVillageMusic.Stop();
         finalBossMusic.Play();
-        missionCanvas.SetActive(false);
+        GameObject.Find("InnerButton").GetComponent<ButtonFader>().enabled = false;
     }
 
+    IEnumerator errorTxtBlink1()
+    {
+        errorTxt.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine("errorTxtBlink2");
+    }
+
+    IEnumerator errorTxtBlink2()
+    {
+        errorTxt.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        StartCoroutine("errorTxtBlink1");
+    }
 }
--- Assets/Main Assets/C# Scripts/General Scripts/TimeMachineSpawn.cs
+++ Assets/Main Assets/C# Scripts/General Scripts/TimeMachineSpawn.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class TimeMachineSpawn : MonoBehaviour
 {
-    public GameObject timeMachine;
+    public GameObject timeMachine, TMSpawnEffect;
 
-    void FixedUpdate()
+    void Update()
     {
         if (CheckWyverns2.AllWyvernsDead2 == true && CheckHydras2.AllHydrasDead2 == true && CheckGoblinsChildren.AllGoblinsDead == true)
         {
             timeMachine.SetActive(true);
+            TMSpawnEffect.SetActive(true);
         }
     }
 }
